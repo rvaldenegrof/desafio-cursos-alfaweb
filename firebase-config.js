ef520d01612770c6fbf81ea43177f3d1eb61ec00
rvaldenegrof <r.valdenegrof@gmail.com>
2021-09-23 12:46:15 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyC3RXQt36rJLzcLQFN9hSWE35gC7NrbjvU',
-  authDomain: 'test-7dae3.firebaseapp.com',
-  projectId: 'test-7dae3',
-  storageBucket: 'test-7dae3.appspot.com',
-  messagingSenderId: '1093438907037',
-  appId: '1:1093438907037:web:7a7e2247878f974827ea74'
-}
